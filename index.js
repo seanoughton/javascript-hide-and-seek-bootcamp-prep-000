da4cafd3aa370867c7b6017533162611be26a872
Sean Oughton <seanoughton@gmail.com>
2017-12-27 22:31:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector){
+  const firstElement = document.querySelector(selector)
+  return firstElement
+}
+
+function nestedTarget() {
+  var something = document.querySelector('#nested .target')
+  return something
+}
+
+function increaseRankBy(n) {
+  var rankedLists = document.querySelectorAll('.ranked-list')
+  for (let i =0; i<rankedLists.length; i++){
+    rankedLists[i].innerHTML = (parseInt(rankedLists[i].innerHTML) + n).toString()
+  }
+}
+
+function deepestChild(){
+  var nodeList = document.getElementById('grand-node').querySelectorAll('div')
+  for (let i=0; i < nodeList.length; i++){
+
+  }
+
+}
